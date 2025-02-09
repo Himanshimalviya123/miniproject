684e01925e6549130baa7ed2368582f63960a7e6
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-09 19:58:54 +0530

    modify

--- practice.js
+++ practice.js
@@ -15,3 +15,38 @@ function solve(){
  let y = eval(x);
  document.querySelector("#display").value=y;
 }
+// /=====================================
+let store=""
+let cal=(value)=>{
+    store=store+value   //1  =>"12+5"
+    let show=document.querySelector("#display")
+    show.innerHTML=store
+}
+let res=()=>{
+    let show=document.querySelector("#display")
+     show.innerHTML=eval(store)   //"12+5"   =>17 number
+     store=eval(store)
+    // method(to string )============== convert number into string
+    store=toString()//17 string
+}
+// =====================================all clear===========================
+let ac=()=>{  
+store=""
+let show=document.querySelector("#display")
+     show.innerHTML=(store)
+     
+
+
+}
+
+// =========================delete **(slice) string method========================== 
+let del=()=>{
+  store=store.slice(0,-1)
+  let show=document.querySelector("#display")
+  show.innerHTML=(store)
+}
+let sq=()=>{
+  store=store*2
+  let show=document.querySelector("#display")
+  show.innerHTML=(store)
+}
