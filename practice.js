6fb2797dac7d606a2ce2d3e743bb82a9a66cb35d
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-09 19:46:16 +0530

    update

--- practice.js
+++ practice.js
@@ -1,34 +1,17 @@
-let store=""
-let cal=(value)=>{
-    store=store+value   //1  =>"12+5"
-    let show=document.querySelector("#display")
-    show.innerHTML=store
-}
-let res=()=>{
-    let show=document.querySelector("#display")
-     show.innerHTML=eval(store)   //"12+5"   =>17 number
-     store=eval(store)
-    // method(to string )============== convert number into string
-    store=toString()//17 string
-}
-// =====================================all clear===========================
-let ac=()=>{  
-store=""
-let show=document.querySelector("#display")
-     show.innerHTML=(store)
-     
-
-
+function calcul(val){
+  document.querySelector("#display").value += val;
 }
 
-// =========================delete **(slice) string method========================== 
-let del=()=>{
-  store=store.slice(0,-1)
-  let show=document.querySelector("#display")
-  show.innerHTML=(store)
+function clr(){
+  document.querySelector("#display").value = " ";
 }
-let sq=()=>{
-  store=store*2
-  let show=document.querySelector("#display")
-  show.innerHTML=(store)
+
+function DE(){
+  document.querySelector("#display").value = document.querySelector("#display").value.slice(0,-1);
+}
+
+function solve(){
+ let x= document.querySelector("#display").value;
+ let y = eval(x);
+ document.querySelector("#display").value=y;
 }
