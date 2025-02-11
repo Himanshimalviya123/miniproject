5f0591c8251746cf62c9359055bdd86212fc9c7c
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-11 22:53:39 +0530

    update

---
+++ settimeout.js
@@ -0,0 +1,77 @@
+// ===================================(setTimeout)===========
+// setTimeout(()=>{
+//     console.log("running...");
+// },3000)
+// console.log("in proccess");
+// let abc()=>{
+// setTimeout(()=>{
+//     console.log("<h1>himanshi</h1>");
+// },3000)
+// }
+// let abc=()=>{
+//  let pvc=document.querySelector=("#cat")
+//  setTimeout(()=>{
+// pvc.innerHTML="himanshi"
+// },3000)
+// }
+// // let 
+
+// ========================( setInterval )===============================
+// let count=0;
+// setInterval(()=>{
+//     console.log(count);
+//     count++;
+//  },2000)
+// let abc=()=>{
+// let count=0;
+// let abc=()=>{
+//     let pvc=document.querySelector=("#cat")
+// setInterval(()=>{
+//     // let pvc=document.querySelector=("#cat")
+//     pvc.innerHTML=count;
+//     count++;
+//  },2000)
+// }
+// ========================= (stop )========
+// let inter;
+// let count=0;
+// let inter=setInterval(()=>{
+//     console.log(count);
+//     count++;
+//                                                 //console
+// },2000)
+
+
+// let stopp=()=>{
+//     clearInterval(inter)
+// }
+// ===============================================
+// let count=0;
+// let stopp=()=>{
+//     let efg=document.querySelector=("#number")
+
+//     setInterval=(()=>{
+//         efg.innerHTML=Count
+//         count++;
+//     },2000)
+// }
+// let count=0;
+// setInterval(()=>{
+//     console.log(count);                     //console prrr
+//     count++
+// },2000)
+
+//  let clear;
+let count=0;
+let abc=()=>{
+    let show=document.querySelector("#number").value
+  setInterval(()=>{
+    show.innerHTML=count;                     //Page prr
+    count++
+},2000)
+if(show>=number)
+    alert(number)
+count++
+
+
+}
